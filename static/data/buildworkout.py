27bb8aa2930737f778e5702f40171a6640b95adf
Sylvia Fechner <Fechner@C02VD097HV2V.local>
2020-05-07 18:45:48 -0700

    first complete workout

--- static/data/buildworkout.py
+++ static/data/buildworkout.py
@@ -6,16 +6,20 @@ with open('ExerciseList.json') as f:
 exerlist = exerlist['exercises']
 
 
-exercises = ['Inner Sole Tap', 'Squat and Knee Tap','Frog Squats','10 High Knee & 3 Tuck Jumps','Crunches', 'Mountain Climber']
+
+exercises = ['Squat Walk', 'Jumping Lunges', 'Burpees',
+'Plank','Sit Up','Inner Sole Tap', 
+'Squat and Knee Tap','Rope Jump','Frog Squats',
+'10 High Knee & 3 Tuck Jumps','Crunches', 'Mountain Climber']
 #exercises = ['Inner Sole Tap', 'Squat and Knee Tap']
 
 #todo: write the same code for warm up list
 
-warmups = ['Jumping Jacks', 'Single Leg Hip Circles', 'Squat Pulses', 'Glute bridge', '5 shoulder circles + 2 wind mills', 'Burpees']
+warmups = ['Jumping Jacks', 'Butt Kicker', 'Squat Pulses','5 Shoulder Circles & Windmill','Burpees']#'Glute bridge','Single Leg Hip Circles', '5 shoulder circles + 2 wind mills',
 
-duration_work = 5
-duration_rest = 3
-duration_warmup = 2
+duration_work = 40
+duration_rest = 10
+duration_warmup = 25
 
 
 arrayelements = [{
@@ -28,51 +32,89 @@ arrayelements = [{
     },
     {
       "id": 1,
-      "heading": "Introduction to workouts",
+      "heading": "Introduction to workouts: 40 work, 10 rest, 3 rounds",
       "name": exercises,
-      "duration": 10,
+      "duration": 5,
       "gifpath": "",
       "sound": None
-    },
-    {
-      "id": 2,
+    }]
+
+##### introduction to workout
+n = 2
+
+for i in range(len(exercises)):
+
+    exer_index = next((index for (index, d) in enumerate(exerlist) if d["name"] == exercises[i]), None)
+    exer_elem = exerlist[exer_index]
+
+    wo1 =     {
+        "id": n,
+        "heading": "Introduction to workouts",
+        "name": exer_elem['name'],
+        "duration": 10,
+        "gifpath": exer_elem['gifpath'],
+        "sound": None
+      }
+    
+
+    arrayelements.append(dict(wo1))
+    
+    n = n+1
+    print(n)
+
+getreadywu = {
+      "id": n,
       "heading": "Get Ready to Warm Up",
       "name": " Let's go !!!!! ",
       "duration": 10,
       "gifpath": "static/movie/getready.mp4",
       "sound": None
-    },
-    {
-      "id": 3,
-      "heading": "Warm Up",
-      "name": "Jumping Jacks",
-      "duration": duration_warmup,
-      "gifpath": "static/movie/JumpingJack.mp4",
-      "sound": 'audiowork'
-    },
+    }
 
-    {
-      "id": 4,
-      "heading": "Warm Up",
-      "name": "Squat Pulses",
-      "duration": duration_warmup,
-      "gifpath": "static/movie/SquatPulses.mp4",
-      "sound": 'audiowork'
-    },
-
-    {
-      "id": 5,
-      "heading": "Warm Up",
-      "name": "5 Shoulder Circles & Windmill",
-      "duration": duration_warmup,
-      "gifpath": "static/movie/ShouldrCircles.mp4",
-      "sound": 'audiowork'
-    }]
+arrayelements.append(dict(getreadywu))
+ 
+n = n +1 
 
 
+#write part for warm up
+for i in range(len(warmups)):
+    #print(len(warmups))
+    exer_index = next((index for (index, d) in enumerate(exerlist) if d["name"] == warmups[i]), None)
+    exer_elem = exerlist[exer_index]
+    #print(exer_index)
+    #print(exer_elem)
+    wu =     {
+        "id": n,
+        "heading": "Warm Up",
+        "name": exer_elem['name'],
+        "duration": duration_warmup,
+        "gifpath": exer_elem['gifpath'],
+        "sound": 'audiowork'
+      }
+    
+    arrayelements.append(dict(wu))
+    
+    n = n+1
+    print(n)
+
+
+
+
+getreadywork = {
+      "id": n,
+      "heading": "Get Ready to Work Out",
+      "name": " Let's go !!!!! ",
+      "duration": 5,
+      "gifpath": "static/movie/getready.mp4",
+      "sound": None
+    }
+
+arrayelements.append(dict(getreadywork))
+ 
+n = n +1 
+print(n)
 
 ################# TIMING 3 repeats ##### todo: add script for different timing
-n = 6
 
 rounds = 3
 
@@ -117,7 +159,7 @@ while (rounds > 0):
 		water =     {
 		      "id": n,
 		      "heading": "Water Break",
-		      "name": "Up Next"+ exercises[0],
+		      "name": "Up Next: "+ exercises[0],
 		      "duration": 4,
 		      "gifpath": "static/movie/WaterRefill.mp4",
           "sound": 'audiorest'
@@ -132,7 +174,7 @@ while (rounds > 0):
 
 workout = {'startTime': 'now', 'elements': arrayelements}
 
-print(workout)
+#print(workout)
 
 with open('workout1.json', 'w') as fp:
     json.dump(workout, fp)
