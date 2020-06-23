997408ced61650a0125fcd1ddd03973a1499584a
raphael <raphael@dexterity.ai>
2020-06-22 22:27:39 -0700

    added badges

--- static/js/selection.js
+++ static/js/selection.js
@@ -1,9 +1,8 @@
 var number_of_workouts = 0;
 $(document).ready(function () {
     //Create time for the datetimepicker to set -> now plus 2 minutes
-    var now = new Date();
-    var now_in_2 = new Date().setMinutes(now.getMinutes()+2);
 
+    var now_in_2 = new Date(getDateIn2Minutes());
 
     $('#datetimepicker1').datetimepicker({
         // format: 'YYYY/MM/DD'
@@ -18,7 +17,7 @@ $(document).ready(function () {
             today: 'far fa-calendar-check',
             clear: 'far fa-trash-alt',
             close: 'fas fa-times'
-        },defaultDate: now_in_2
+        },defaultDate: now_in_2,minDate: now_in_2
     });
 
 
@@ -29,6 +28,9 @@ $(document).ready(function () {
         })
         .then((data) => {
             number_of_workouts = data['exercises'].length
+            //Make data global available
+            window.data = data
+
             parseExercisesToForm(data);
         });
 });
@@ -62,6 +64,7 @@ function generateRandomWorkout(number) {
 }
 
 function parseExercisesToForm(data) {
+
     // Filter for categories
     const belly_list = data['exercises'].filter(excercise => excercise.category.includes("belly"));
     const legs_list = data['exercises'].filter(excercise => excercise.category.includes("legs"));
@@ -76,55 +79,120 @@ function parseExercisesToForm(data) {
             $('<div class="col-md-4 mb-3 input-check-exercise"' +
                 '>' +
                 '<div class="form-check">' +
-                ' <input type="checkbox" class="form-check-input" name="' + elem.name + '" id="exercise-' + elem.id + '">' +
+                ' <input type="checkbox" class="form-check-input" name="' + elem.id + '" id="exercise-' + elem.id + '">' +
                 '   <label class="form-check-label" for=excercise-' + elem.id + '>' + elem.name + '</br></label>' +
                 '</div></div>').appendTo(wrapper);
         });
-        wrapper.appendTo("#excercises-boxes")
+        wrapper.appendTo("#excercises-boxes");
     });
+}
+function getDateIn2Minutes() {
+    var now = dayjs()
+    var now_plus_2 = now.add(2,'minute');
+    return now_plus_2;
 }
 
 function submitcheck(element) {
     var selected_rounds = element[0].options[element[0].selectedIndex].value;
-    console.log(selected_rounds)
-    var selected_duration = element[1].value;
-    console.log(selected_duration)
-    var selected_date = element[2].value;
+    var duration_wo = element[1].value;
+    var duration_rest = element[2].value;
+    var selected_date = element[3].value;
     console.log(selected_date)
     var selected_elements = $(element).serializeArray();
     console.log(selected_elements)
-    exercise_list = [];
+    exercise_id_list = [];
     $.each(selected_elements, function (index, element) {
-        exercise_list.push(element.name)
+        exercise_id_list.push(element.name)
     });
 
     // Generate HTML List from exercise list for summary
-    ul = document.createElement('ul')
-    ul.classList.add("list-group");
-    console.log("excercise",exercise_list)
-    exercise_list.forEach(function (item) {
-        let li = document.createElement('li');
-        li.classList.add('list-group-item')
+    var table_container = document.createElement('div')
+    ul = document.createElement('div')
+    ul.classList.add("row");
+    ul.classList.add("m-3");
+    var name_for_id = window.data.exercises;
+    var cat_list = []
+    exercise_id_list.forEach(function (item) {
+        var excercise_obj = name_for_id.filter(obj => {
+            return obj.id === parseInt(item)
+        })[0]
+        cat_list.push(excercise_obj.category)
+
+        let li = document.createElement('div');
+        //li.classList.add('list-group-item')
+        li.classList.add('col-md-4')
+        li.classList.add('border')
+        li.classList.add('d-flex')
+        li.classList.add('p-3')
+        li.classList.add('justify-content-center')
         li.style.backgroundColor = "#555"
         ul.appendChild(li);
-        li.innerHTML += item;
+        li.innerHTML += excercise_obj.name;
     });
+    table_container.append(ul)
+
+
     $('.modal-body').empty();
     $('.modal-body').append("<h5>Summary</h5>");
-    $('.modal-body').append(ul);
-    createModal(exercise_list,selected_date,selected_duration,selected_rounds);
+    $('.modal-body').append(table_container);
+
+    var counts = {}
+    cat_list.forEach((el) => {
+        counts[el] = counts[el] ? (counts[el] += 1) : 1;
+    });
+    console.log(counts)
+
+    for (const [key, value] of Object.entries(counts)) {
+        let div = document.createElement('div');
+        div.classList.add("btn")
+        div.classList.add("btn-primary")
+        div.classList.add("m-2")
+        div.classList.add("disabled")
+        div.innerHTML = key
+        let span = document.createElement('span');
+        span.classList.add("badge")
+        span.classList.add("badge-light")
+        span.classList.add("ml-2")
+        span.innerHTML = value
+        div.append(span)
+        $('.modal-body').append(div)
+        console.log(`${key}: ${value}`);
+    }
+
+
+
+
+
+    //create object for the url
+    var url_builder_obj = {}
+    url_builder_obj["excercises"] = JSON.stringify(exercise_id_list)
+    var selected = dayjs(selected_date.toString())
+    // Check if selected date is over. If so, take now and add 2 minutes for starting time
+    if(selected.isBefore(dayjs())){
+        console.log("add 2 minures")
+        selected_date = getDateIn2Minutes().toLocaleString()
+    }
+    url_builder_obj["timestamp"] = selected_date
+    url_builder_obj["wo_duration"] = duration_wo
+    url_builder_obj["rest_duration"] = duration_rest
+    url_builder_obj["wo_rounds"] = selected_rounds
+
+    createModal(url_builder_obj);
     return false;
 }
 
-function createModal(exercises, timestamp, duration,rounds) {
-    //Here workout.json should be created and saved in jsonbin.io
+function createModal
+(url_object) {
+    //Create url from object
+    var url_parameters = $.param(url_object);
+
+    //Show modal
     $('.modal').modal();
-    if(timestamp=="now"){
-        timestamp= dayjs().add(20,'seconds').toLocaleString();
-    }
-    $('#btn-go-to-workout').attr("href", "workout.html?excercises=" + encodeURIComponent(JSON.stringify(exercises)) + "&timestamp=" + timestamp +"&wo_duration=" + duration + "&wo_rounds="+rounds);
+
+    //Add link to created workout to button and clipboard
+    $('#btn-go-to-workout').attr("href", "workout.html?"+url_parameters);
     var clipboard = new ClipboardJS('.clipboard-button');
-    document.getElementById('next-0-link').value = window.location.hostname+"/workout.html?excercises=" + encodeURIComponent(JSON.stringify(exercises)) + "&timestamp=" + timestamp +"&wo_duration=" + duration + "&rounds="+rounds;
+    document.getElementById('next-0-link').value = window.location.hostname+"/workout.html?"+url_parameters;
 
 }
 
