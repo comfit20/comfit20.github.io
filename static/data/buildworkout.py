b41e9248add35f09b91bb37dc098a0335696abe1
Sylvia Fechner <Fechner@C02VD097HV2V.local>
2020-06-23 20:26:54 -0700

    Wednesday workout

--- static/data/buildworkout.py
+++ static/data/buildworkout.py
@@ -7,8 +7,8 @@ with open('ExerciseList.json') as f:
 exerlist = exerlist['exercises']
 
 
-exercises = ['6 Fast Feet In & Out', 'Mountain Climber', 'Rope Jump', 'Static Spiderman', 'Bear Crawl', 'Jack Knife', 'Sumo Squat',
- 'Burpees',  'Crunches', '10 Fast Feet & Push Up', 'Plank', 'Jump Squat (+ Touch)'] 
+exercises = ['High Knees', 'Squat & Knee Rise', 'Low & High Plank Switch', 'Single-Leg Glute Bridge', 'Sit Up', 'Superman (alt: Pull Ups)','Squat Walk', 
+ 'Diamond Push Up', 'Leg Lift & Hold',  '10 Fast Feet & Jump', 'Side Plank Crunches' ,'Ladder (alt: Fast Feet)',]
 
 
 
@@ -25,7 +25,7 @@ warmups = ['Jumping Jacks', 'Single Leg Hip Circles',  'Squat Pulses','Glute Bri
   #repeat_diff erenttimes 9 exercise 45 + 25 + 45 + 25 (1260);   no water break
   #'up_and_down'. 30 -60 - 30: 12 exercises
 
-workoutstyle = 'regular' # change workout styles here
+workoutstyle = 'decrease' # change workout styles here
 
 duration_warmup = 25
 applaus = random.randint(1,2)
