6931db04fea13f4e4b9242758e904e1dc4a6cd33
sfechner <sylvia.fechner@gmx.net>
2020-10-07 11:42:30 +0200

    added jitsii survey

--- static/js/workout.js
+++ static/js/workout.js
@@ -244,7 +244,11 @@ function parseResults(data) {
 function startJqueryTimer(startTime) {
     if (startTime['elements'].length == 0) {
         $("#content").empty()
-        $("#content").html('<h1>!!! Workout over !!!!</h1>')
+        $("#content").html('<h2>!!! Good Job: Workout over !!!')
+        $("#content").append('<h4> We kindly ask you for feedback </h4>')
+        $("#content").append('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdP8wpxa9pXAzYhNkFlHjpi_I2GPAEQGvGbUTUEHbzerTE0Uw/viewform?embedded=true" width="450" height="850" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>');
+
+
         return
     }
 
