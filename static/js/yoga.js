36dce9894c27d2892fb48496b4ca5039d1ae1ae5
zabinsky <rzabinsky@gmail.com>
2020-11-12 12:11:32 -0800

    edit js

--- static/js/yoga.js
+++ static/js/yoga.js
@@ -15,6 +15,43 @@ var audio_mute = true;
 
 
 $(document).ready(function () {
+
+// Try to start jitsi if embedded, if not embedded (e.g. in workout.html and not workout_group.html catch exception
+//  to go on with execution of script.)
+try {
+    const domain = 'comfit.fun';
+    const options = {
+        roomName: 'Comfit_Group_Yoga_Room',
+        parentNode: document.querySelector('#jitsi')
+    };
+    const api = new JitsiMeetExternalAPI(domain, options);
+      // Make tileview default: From https://github.com/jitsi/jitsi-meet/issues/5764
+      api.addEventListener(`videoConferenceJoined`, () => {
+    const listener = ({ enabled }) => {
+      api.removeEventListener(`tileViewChanged`, listener);
+
+      if (!enabled) {
+        api.executeCommand(`toggleTileView`);
+      }
+    };
+
+    api.addEventListener(`tileViewChanged`, listener);
+    api.executeCommand(`toggleTileView`);
+    api.on('readyToClose', () => { //Hack: Hide jitsi frame after exit meeting to get rid of regular jitsi page
+    // see https://community.jitsi.org/t/redirect-url-on-hangup/29948/23
+    $('#jitsi').hide();
+});
+  });
+}
+catch (e) {
+   // Exception caught. Happens in workout.html since script is not embedded.
+   // console.log(e); // Fehler-Objekt an die Error-Funktion geben
+}
+
+    calcOffset();
+});
+
+function startSiteBuilding() {
     //Get workoutfile from URL, if not there take yoga1.json as default
     // If workoutfile is specified, load it from the file
     var workoutFile = "yoga1.json";
