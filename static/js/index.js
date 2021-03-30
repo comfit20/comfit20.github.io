c8763e68d35f116b18ab1fc0f065bd0856cb0074
comfit20 <64983366+comfit20@users.noreply.github.com>
2021-03-30 19:07:49 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -41,7 +41,7 @@
         var time_2 = getNextWeekday(2).set('h',1).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_2); // this is Monday 6:00 pm
 
-        var time_4 = getNextWeekday(2).set('h',18).set('m',2).set('s',0).set('ms',0);
+        var time_4 = getNextWeekday(2).set('h',17).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_4); // this is Tuesday 10:00 am  
 
         var time_3 = getNextWeekday(3).set('h',14).set('m',2).set('s',0).set('ms',0);
@@ -117,7 +117,7 @@ function createYogaTimeList() {
           time_test = {
           time_Mon: getNextWeekday(1).set('h',15).set('m',2).set('s',0).set('ms',0),
           time_Mon2: getNextWeekday(2).set('h',1).set('m',2).set('s',0).set('ms',0),
-          time_Tue: getNextWeekday(2).set('h',18).set('m',2).set('s',0).set('ms',0),
+          time_Tue: getNextWeekday(2).set('h',17).set('m',2).set('s',0).set('ms',0),
           time_Wed: getNextWeekday(3).set('h',14).set('m',2).set('s',0).set('ms',0),
           time_Thu: getNextWeekday(4).set('h',18).set('m',2).set('s',0).set('ms',0),
           time_Fri: getNextWeekday(5).set('h',14).set('m',2).set('s',0).set('ms',0)
