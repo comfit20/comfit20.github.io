8fc7128908ff830b67aa6799494f5f5310fedfcf
sfechner <sylvia.fechner@gmx.net>
2023-03-20 09:52:34 -0700

    added week March 20th 2023

--- static/js/index.js
+++ static/js/index.js
@@ -40,7 +40,7 @@
         time_list.push(time_1); //this is Monday 7:00 am
         var time_2 = getNextWeekday(2).set('h',1).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_2); // this is Monday 6:00 pm
-        var time_7 = getNextWeekday(2).set('h',16).set('m',15).set('s',0).set('ms',0);
+        var time_7 = getNextWeekday(2).set('h',15).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_7); // this is  Tuesday 7:00 am
 
         var time_4 = getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0);
@@ -53,7 +53,7 @@
         time_list.push(time_6); // this is Thursday 7:00 am  
 
         var time_5 = getNextWeekday(5).set('h',14).set('m',2).set('s',0).set('ms',0);
-        time_list.push(time_5); //this is Friday 8:30 am
+        time_list.push(time_5); //this is Friday 7:02 am
 
         // var time_6 = getNextWeekday(0).set('h',16).set('m',34).set('s',0).set('ms',0);
         // time_list.push(time_6);
@@ -119,7 +119,7 @@ function createYogaTimeList() {
           time_test = {
           time_Mon: getNextWeekday(1).set('h',14).set('m',2).set('s',0).set('ms',0),
           time_Mon2: getNextWeekday(2).set('h',1).set('m',2).set('s',0).set('ms',0),
-          time_Tue7am: getNextWeekday(2).set('h',16).set('m',15).set('s',0).set('ms',0),
+          time_Tue7am: getNextWeekday(2).set('h',15).set('m',2).set('s',0).set('ms',0),
           time_Tue: getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0),
           time_Wed: getNextWeekday(3).set('h',16).set('m',32).set('s',0).set('ms',0),
           time_Thu: getNextWeekday(4).set('h',17).set('m',2).set('s',0).set('ms',0),
