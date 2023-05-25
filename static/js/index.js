3d0bdbedb3ad4d103c50f23af997e7869e3ab6fa
sfechner <sylvia.fechner@gmx.net>
2023-05-24 20:22:44 -0700

    added fridays workout

--- static/js/index.js
+++ static/js/index.js
@@ -43,14 +43,14 @@
         var time_7 = getNextWeekday(2).set('h',14).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_7); // this is  Tuesday 7:00 am
 
-        var time_4 = getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0);
-        time_list.push(time_4); // this is Tuesday 10:00 am  
+        // var time_4 = getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0);
+        // time_list.push(time_4); // this is Tuesday 10:00 am  
 
-        var time_3 = getNextWeekday(3).set('h',16).set('m',32).set('s',0).set('ms',0);
-        time_list.push(time_3); //this is Wednedsay 7:00 am
+        // var time_3 = getNextWeekday(3).set('h',16).set('m',32).set('s',0).set('ms',0);
+        // time_list.push(time_3); //this is Wednedsay 7:00 am
 
-        var time_6 = getNextWeekday(4).set('h',17).set('m',2).set('s',0).set('ms',0);
-        time_list.push(time_6); // this is Thursday 7:00 am  
+        // var time_6 = getNextWeekday(4).set('h',17).set('m',2).set('s',0).set('ms',0);
+        // time_list.push(time_6); // this is Thursday 7:00 am  
 
         var time_5 = getNextWeekday(5).set('h',14).set('m',2).set('s',0).set('ms',0);
         time_list.push(time_5); //this is Friday 7:02 am
@@ -120,9 +120,9 @@ function createYogaTimeList() {
           // time_Mon: getNextWeekday(1).set('h',14).set('m',2).set('s',0).set('ms',0),
           time_Mon2: getNextWeekday(2).set('h',1).set('m',32).set('s',0).set('ms',0),
           time_Tue7am: getNextWeekday(2).set('h',14).set('m',2).set('s',0).set('ms',0),
-          time_Tue: getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0),
-          time_Wed: getNextWeekday(3).set('h',16).set('m',32).set('s',0).set('ms',0),
-          time_Thu: getNextWeekday(4).set('h',17).set('m',2).set('s',0).set('ms',0),
+          // time_Tue: getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0),
+          // time_Wed: getNextWeekday(3).set('h',16).set('m',32).set('s',0).set('ms',0),
+          // time_Thu: getNextWeekday(4).set('h',17).set('m',2).set('s',0).set('ms',0),
           time_Fri: getNextWeekday(5).set('h',14).set('m',2).set('s',0).set('ms',0)
       }
 
