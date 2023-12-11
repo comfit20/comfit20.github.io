8b07edf044ccb6541ee566a16ae4756b65359c86
sfechner <sylvia.fechner@gmx.net>
2023-12-10 16:46:33 -0800

    added download button permanently visible

--- static/js/workout_generator.js
+++ static/js/workout_generator.js
@@ -235,7 +235,7 @@ function generateWorkoutJson(duration_work, rounds, exerjson, exercises, duratio
             "id": n,
             "heading": "Plank Challenge",
             "name": " Plank ",
-            "duration": 120,
+            "duration": 300,
             "gifpath": "static/movie/Plank.mp4", //+str(applaus)+".mp4",
             "sound": 'audiowork',
             "indicator": "hidden"
