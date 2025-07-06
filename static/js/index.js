4301eed059f123de36900dac32d2edbecb9c77ff
sfechner <sylvia.fechner@gmx.net>
2025-07-06 20:50:24 +0200

    added monday 1 workout

--- static/js/index.js
+++ static/js/index.js
@@ -36,8 +36,8 @@
     function createWorkoutTimeList() {
         var time_list = []
 
-        // var time_1 = getNextWeekday(1).set('h',14).set('m',2).set('s',0).set('ms',0);
-        // time_list.push(time_1); //this is Monday 7:00 am
+        var time_1 = getNextWeekday(1).set('h',18).set('m',2).set('s',0).set('ms',0);
+        time_list.push(time_1); //this is Monday 7:00 am
         var time_2 = getNextWeekday(2).set('h',1).set('m',32).set('s',0).set('ms',0);
         time_list.push(time_2); // this is Monday 6:00 pm
         var time_7 = getNextWeekday(2).set('h',14).set('m',2).set('s',0).set('ms',0);
@@ -117,7 +117,7 @@ function createYogaTimeList() {
 
                 var time_test = Object();
           time_test = {
-          // time_Mon: getNextWeekday(1).set('h',14).set('m',2).set('s',0).set('ms',0),
+          time_Mon: getNextWeekday(1).set('h',18).set('m',2).set('s',0).set('ms',0),
           time_Mon2: getNextWeekday(2).set('h',1).set('m',32).set('s',0).set('ms',0),
           time_Tue7am: getNextWeekday(2).set('h',14).set('m',2).set('s',0).set('ms',0),
           // time_Tue: getNextWeekday(2).set('h',18).set('m',32).set('s',0).set('ms',0),
@@ -143,9 +143,9 @@ function createYogaTimeList() {
 
             if (_.isEqual(time_test["time_Mon"], time_list[0]) == true)  {
             //var share_link = window.location.hostname+'/workout.html?workout=workout'+day_workout+'.json'+'&timestamp='+time.toISOString();
-            var share_link = window.location.hostname+'/workout_group.html?workout=workout'+'_weights'+'.json'+'&timestamp='+time.toISOString();
+            var share_link = window.location.hostname+'/workout_group.html?workout=workout'+'_mon1'+'.json'+'&timestamp='+time.toISOString();
             //$("#next-"+idx).attr('href','workout.html?timestamp='+time.toISOString()+"&workout=workout"+day_workout+".json");
-            $("#next-"+idx).attr('href','workout_group.html?timestamp='+time.toISOString()+"&workout=workout"+'_weights'+".json");
+            $("#next-"+idx).attr('href','workout_group.html?timestamp='+time.toISOString()+"&workout=workout"+'_mon1'+".json");
             $('#next-'+idx+'-link').attr('value',share_link);}
 
             else if (_.isEqual(time_test["time_Tue7am"], time_list[0]) == true)  {
